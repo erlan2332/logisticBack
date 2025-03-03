c466cd1c1cb4b024bfa7450364f61297db1f2ca7
erlan2332 <134258930+erlan2332@users.noreply.github.com>
2025-03-03 15:10:38 +0600

    Update TruckRepository.java

--- src/main/java/com/example/demo/repository/TruckRepository.java
+++ src/main/java/com/example/demo/repository/TruckRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TruckRepository extends JpaRepository<Truck, Long> {
-    @EntityGraph(attributePaths = {"trips"}) // 🔹 Загружаем trips сразу
+    @EntityGraph(attributePaths = {"trips"}) 
     Optional<Truck> findWithTripsById(Long id);
 }
